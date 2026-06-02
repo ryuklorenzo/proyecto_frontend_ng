2f31635930b18703fb2f0e23877961790bd998f4
AzaelAcin33 <pitilin2424@gmail.com>
2026-06-02 12:05:51 +0200

    FIX: se me olvido poner una cosa.

--- src/app/core/services/aula-convivencia/aula-convivencia.spec.ts
+++ src/app/core/services/aula-convivencia/aula-convivencia.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ClassroomService } from './mention';
+import { ClassroomService } from './aula-convivencia';
 
 describe('Student', () => {
   let service: ClassroomService;
