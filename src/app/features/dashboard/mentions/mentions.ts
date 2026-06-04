cee8338ac92f0bcc4501df2271669a1be4de398f
ryuk_Lorenzo <angel14lpl@gmail.com>
2026-06-05 01:00:03 +0200

    FEAT: mentions creado vacio, por hacer

--- src/app/features/dashboard/mentions/mentions.ts
+++ src/app/features/dashboard/mentions/mentions.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
+import { AuthService, UserRole } from '../../../core/auth/auth';
+import {
+  LucideAward,
+  LucideBadgeCheck,
+  LucideSearch,
+  LucidePencil,
+  LucideTrash2,
+  LucideLogOut,
+  LucideChevronLeft,
+  LucideChevronRight,
+  LucideUserCircle,
+  LucideDynamicIcon,
+} from '@lucide/angular';
+import { CommonModule } from '@angular/common';
+
+interface Buttons {
+  label: string;
+  icon: any;
+  roles: UserRole[];
+}
 
 @Component({
   selector: 'app-mentions',
-  imports: [],
+  imports: [LucideDynamicIcon, CommonModule],
   templateUrl: './mentions.html',
   styleUrl: './mentions.css',
 })
-export class Mentions {}
+export class Mentions {
+  authService = inject(AuthService);
+  user = this.authService.user;
+
+  icons = {
+    ChevronLeft: LucideChevronLeft,
+    ChevronRight: LucideChevronRight,
+    UserCircle: LucideUserCircle,
+    LogOut: LucideLogOut
+  }
+
+  private butonItems: Buttons[] = [
+    { label: 'Crear mención', icon: LucideAward, roles: ['admin', 'directivo'] },
+    { label: 'Ver menciones', icon: LucideBadgeCheck, roles: ['admin', 'directivo', 'profesor'] },
+    { label: 'Ver menciones por reconocimiento', icon: LucideSearch, roles: ['admin', 'directivo'] },
+  ];
+
+  filteredButtons = computed(() => {
+    const currentUser = this.user();
+    if (!currentUser) return [];
+    return this.butonItems.filter((btn) => btn.roles.includes(currentUser.role));
+  });
+}
