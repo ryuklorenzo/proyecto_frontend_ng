b291b2e79c2af6ef489345eed1027ed580b800b8
ryuk_Lorenzo <angel14lpl@gmail.com>
2026-06-09 13:46:04 +0200

    FEAT: codigo comentado

--- src/app/features/dashboard/classroom_coexistence/classroom_coexistence.ts
+++ src/app/features/dashboard/classroom_coexistence/classroom_coexistence.ts
@@ -107,7 +107,7 @@ export class Classroom_coexistence implements OnInit {
     
     // Cruzamos los datos de cada aula con su horario correspondiente
     const aulasDetalladas = allAulas.map((aula: any) => {
-      const horarioAsignado = allHorarios.find(h => h.id === aula.id_horario);
+      const horarioAsignado = allHorarios.find(h => h.id === aula.id_horario); //busca por por el id_horario
       
       return {
         ...aula,
@@ -116,6 +116,7 @@ export class Classroom_coexistence implements OnInit {
           : `Horario ID: #${aula.id_horario}`
       };
     });
+    //devuelve con los datos del horario
 
     if (!term) return aulasDetalladas;
     
@@ -140,15 +141,15 @@ export class Classroom_coexistence implements OnInit {
         id_alumno_real: studentId,
         nombre_completo: realStudent ? `${realStudent.nombre} ${realStudent.apellidos}` : 'Desconocido'
       };
-    });
+    });// devuelve nombre y apellidos + ID
   });
 
+  //form
   classroomForm: FormGroup = this.fb.group({
     nombre: ['', Validators.required],
     fecha: [new Date().toISOString().split('T')[0], Validators.required],
     id_horario: [null, Validators.required]
   });
-
   assignForm: FormGroup = this.fb.group({
     id_aula_convivencia: [null, Validators.required],
     alumnos_ids: [[], Validators.required] 
@@ -176,7 +177,7 @@ export class Classroom_coexistence implements OnInit {
     this.ocultarTodo();
     this.mostrarFormulario.set(true);
     this.cargarDatosBase();
-    this.classroomForm.reset({
+    this.classroomForm.reset({//datos vacios, + fecha actual
       nombre: '',
       fecha: new Date().toISOString().split('T')[0],
       id_horario: null
@@ -197,6 +198,7 @@ export class Classroom_coexistence implements OnInit {
 
     // Cargar alumnos ocupados en aulas para filtrarlos del select
     this.classroomService.getClassrooms().subscribe({
+      //obetener las aulas
       next: (aulas: any) => { 
         let ocupadosTemp: number[] = [];
         let peticionesCompletadas = 0;
@@ -208,9 +210,10 @@ export class Classroom_coexistence implements OnInit {
 
         aulas.forEach((aula: any) => {
           this.classroomService.getStudentsByClassroom(aula.id).subscribe({
+            //pide los alumnos en aulas
             next: (estudiantesDelAula: any) => { 
               const ids = estudiantesDelAula.map((e: any) => e.id_alumno || e.id);
-              ocupadosTemp = [...ocupadosTemp, ...ids];
+              ocupadosTemp = [...ocupadosTemp, ...ids];//añade los id de alumno que encuentra
               peticionesCompletadas++;
               
               if (peticionesCompletadas === aulas.length) {
@@ -325,6 +328,7 @@ export class Classroom_coexistence implements OnInit {
 
     const payload = this.assignForm.value; 
     payload.alumnos_ids = payload.alumnos_ids.map((id: string | number) => Number(id));
+    //para que llegue como lo espera el back
 
     this.classroomService.assignStudents(payload).subscribe({
       next: () => {
